5f497af121eaaea7bfe374783c5df8072400a826
BenardV <patduffy1002@gmail.com>
2021-09-06 20:16:03 +0200

    wordpress 2.0

---
+++ wp-content/uploads/custom-css-js/918.js
@@ -0,0 +1,14 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+/* Default comment here */
+window.onload = function() {
+  document.getElementsByClassName('es-dymanic-content')[0].innerHTML="";
+
+        };
+
+
+
+
+
+</script>
+<!-- end Simple Custom CSS and JS -->
